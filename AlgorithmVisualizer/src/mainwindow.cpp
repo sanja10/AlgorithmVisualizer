96cf536f3d3b2b642d146bfdd0c99587943ed3d0
sanja10 <mijatovicsanja000@gmail.com>
2022-03-13 19:33:55 +0100

    initialized Main Window

---
+++ AlgorithmVisualizer/src/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "include/mainwindow.hpp"
+#include "ui_mainwindow.h"
+#include <iostream>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("Algorithm Visualizer");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_sortButton_clicked()
+{
+
+}
+
---
+++ build-AlgorithmVisualizer-Desktop_Qt_6_2_1_GCC_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,87 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.2.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QPushButton *sortButton;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        MainWindow->setAcceptDrops(false);
+        MainWindow->setStyleSheet(QString::fromUtf8("background-color: rgb(114, 159, 207)"));
+        MainWindow->setDocumentMode(false);
+        MainWindow->setTabShape(QTabWidget::Rounded);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        sortButton = new QPushButton(centralwidget);
+        sortButton->setObjectName(QString::fromUtf8("sortButton"));
+        sortButton->setGeometry(QRect(270, 150, 222, 121));
+        QFont font;
+        font.setPointSize(24);
+        font.setBold(true);
+        font.setItalic(true);
+        font.setStrikeOut(false);
+        sortButton->setFont(font);
+        sortButton->setStyleSheet(QString::fromUtf8("font: 700 italic 24pt \"Ubuntu Mono\" ;\n"
+"background-color: rgb(211, 215, 207);\n"
+"border-style: outset;\n"
+"border-width: 2px;\n"
+"border-color: beige;\n"
+"color: rgb(52, 101, 164);"));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 22));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        sortButton->setDefault(true);
+
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        sortButton->setText(QCoreApplication::translate("MainWindow", "Sort", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
